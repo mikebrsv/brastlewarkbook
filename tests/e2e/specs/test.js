1b61fb79b482eb7dab5cae8b341d2fc814a0bda8
Mikhail Borisov <mikebrsv@gmail.com>
2021-04-18 17:06:01 +0200

    Basic cypress testing

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,33 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
+describe('Index Test', () => {
+  it('Visits the app index url', () => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
+
+    cy.get('.gnome-list-wrapper')
+      .find('.btn')
+      .should('exist')
+      .should('not.be.empty')
+      .contains('Load More')
+
+    cy.get('.gnome-list-wrapper img')
+      .each((i) => {
+        cy.get(i).should('have.attr', 'src').should('include', 'https://')
+        cy.get(i).should('have.attr', 'alt').should('not.be.empty')
+      })
+  })
+})
+
+describe('Profile Test', () => {
+  it('Visits a profile page', () => {
+    cy.visit('/profile/0')
+
+    cy.get('.profile-image-main').invoke('attr', 'alt').then((imgAlt) => {
+      const alt = imgAlt
+
+      cy.get('h1').should((el) => {
+        expect(el.text()).to.eq(alt)
+      })
+    })
   })
 })
